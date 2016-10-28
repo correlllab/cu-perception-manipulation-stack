edb01d922926011ca148c725c40a954e9908a89b
Rebecca <mrevilkangaroo@gmail.com>
2016-10-28 11:11:15 -0600

    Refactored PAP signal code

--- pick_and_place/src/pap/signal_pap.py
+++ pick_and_place/src/pap/signal_pap.py
@@ -13,87 +13,66 @@ class SignalDetector():
         self.calibrate = False
         self.calibrate_vals = [deque(maxlen=100),deque(maxlen=100),deque(maxlen=100)]
 
-        self.object_det_calibrated_pub = rospy.Publisher("/finger_sensor/obj_det_calibrated",
-                                            Bool,
-                                            queue_size=1)
-
-        self.calibrate_obj_det_sub = rospy.Subscriber("/finger_sensor/calibrate_obj_det",
-                                            Bool,
-                                            self.set_calibrate)
-
-        self.fai_sub = rospy.Subscriber("/finger_sensor/fai",
-                                        FingerFAI,
-                                        self.detect_touch)
-
-        self.sai_sub = rospy.Subscriber("/finger_sensor/sai",
-                                        FingerSAI,
-                                        self.detect_object)
-
-        self.object_det_pub = rospy.Publisher("/finger_sensor/obj_detected",
-                                        FingerDetect,
-                                        queue_size=1)
-
-        self.touch_pub = rospy.Publisher("/finger_sensor/touch",
-                                        FingerTouch,
-                                        queue_size=1)
-
-
+        self.object_det_calibrated_pub = rospy.Publisher("/finger_sensor/obj_det_calibrated", Bool, queue_size=1)
+        self.calibrate_obj_det_sub = rospy.Subscriber("/finger_sensor/calibrate_obj_det", Bool, self.set_calibrate)
+        self.fai_sub = rospy.Subscriber("/finger_sensor/fai", FingerFAI, self.detect_touch)
+        self.sai_sub = rospy.Subscriber("/finger_sensor/sai", FingerSAI, self.detect_object)
+        self.object_det_pub = rospy.Publisher("/finger_sensor/obj_detected", FingerDetect, queue_size=1)
+        self.touch_pub = rospy.Publisher("/finger_sensor/touch", FingerTouch, queue_size=1)
 
     def set_calibrate(self,msg):
         self.calibrate = msg.data
+        if msg.data == True:
+            print('Calibrating Fingers...')
 
     def detect_touch(self,msg):
-        touch_msg = FingerTouch()
         touch = self.current_fingers_touch[:]
         fai = [msg.finger1, msg.finger2, msg.finger3]
 
         for finger in range(3):
             if fai[finger] > 2000.0 and self.current_fingers_touch[finger] == False:
                 touch[finger] = True
-
-            if fai[finger] < -2000.0 and self.current_fingers_touch[finger] == True:
+            elif fai[finger] < -2000.0 and self.current_fingers_touch[finger] == True:
                 touch[finger] = False
 
-        if np.any(np.array(self.current_fingers_touch) != np.array(touch)):
-            self.current_fingers_touch = touch
-            touch_msg.finger1 = touch[0]
-            touch_msg.finger2 = touch[1]
-            touch_msg.finger3 = touch[2]
-            self.touch_pub.publish(touch_msg)
-
+        self.current_fingers_touch = self.send_finger_msg(self.touch_pub,FingerTouch,touch,self.current_fingers_touch)
 
     def detect_object(self,msg):
-        detect_msg = FingerDetect()
         detected = self.objectDet[:]
         sai = [msg.finger1, msg.finger2, msg.finger3]
+
         for finger in range(3):
-            if self.calibrate == False:
-                if self.sai_calibration[finger] != [None]*3:
+            if self.calibrate == False and self.sai_calibration[finger] != [None]*3:
                     if sai[finger] > self.sai_calibration[finger] and self.objectDet[finger] == False:
                         detected[finger] = True
-                        self.objectDet[finger] = True
                     elif sai[finger] < self.sai_calibration[finger] and self.objectDet[finger] == True:
                         detected[finger] = False
-                        self.objectDet[finger] = False
             else:
-                if np.all(np.array(self.sai_calibration) != np.array([None]*3)):
-                    self.calibrate=False
-                elif self.calibrate_vals[finger].maxlen == len(self.calibrate_vals[finger]):
-                    self.sai_calibration[finger] = max(self.calibrate_vals[finger]) + 300 #340 for spoon
-                    self.calibrate_vals[finger].clear()
-                    self.object_det_calibrated_pub.publish(True)
-                else:
-                    self.object_det_calibrated_pub.publish(False)
-                    self.calibrate_vals[finger].append(sai[finger])
+                self.calibrate_fingers(sai,finger)
 
-        # if np.any(np.array(self.objectDet) != np.array(detected)):
-        self.objectDet = detected
-        detect_msg.finger1 = detected[0]
-        detect_msg.finger2 = detected[1]
-        detect_msg.finger3 = detected[2]
-        self.object_det_pub.publish(detect_msg)
-        #wut  up
-        
+        self.objectDet = self.send_finger_msg(self.object_det_pub,FingerDetect,detected,self.objectDet)
+
+    def calibrate_fingers(self,data,finger):
+        if np.all(np.array(self.sai_calibration) != np.array([None]*3)):
+            self.calibrate=False
+        elif self.calibrate_vals[finger].maxlen == len(self.calibrate_vals[finger]):
+            self.sai_calibration[finger] = max(self.calibrate_vals[finger]) + 100 #340 for spoon
+            self.calibrate_vals[finger].clear()
+            self.object_det_calibrated_pub.publish(True)
+        else:
+            self.object_det_calibrated_pub.publish(False)
+            self.calibrate_vals[finger].append(data[finger])
+
+    def send_finger_msg(self,publisher,msg_type,data,prev_data):
+        msg = msg_type()
+        if np.any(np.array(prev_data) != np.array(data)):
+            prev_data = data
+            msg.finger1 = data[0]
+            msg.finger2 = data[1]
+            msg.finger3 = data[2]
+            publisher.publish(msg)
+        return data
+
 if __name__=='__main__':
     rospy.init_node('signal_detector')
     sd = SignalDetector()
